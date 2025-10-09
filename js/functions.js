c1bf5ac0ee08d3ebcd93481fc9e4cb971c49fd22
Artur <102910077+ilyachever@users.noreply.github.com>
2025-10-09 22:27:17 +0400

    Добавляет функцию для проверки, является ли строка палиндромом

--- js/functions.js
+++ js/functions.js
@@ -25,3 +25,27 @@ checkIsStringHasValidLength('проверяемая строка', 20);
 checkIsStringHasValidLength('проверяемая строка', 18);
 // Строка длиннее 10 символов
 checkIsStringHasValidLength('проверяемая строка', 10);
+
+/**
+ * Проверяет, является ли строка палиндромом
+ * @param {string} str
+ * @return {boolean} true, если строка является палиндромом, иначе false
+ * @throws {Error} Если аргументы некорректного типа.
+ */
+const checkIsPalindrome = (str = '') => {
+  if (typeof str !== 'string' || !str.length) {
+    throw new Error(`${ErrorMessage.STRING} ${str}`);
+  }
+
+  const normalizedString = str.toLowerCase().replace(/[^a-яёa-z0-9]/gi, '');
+  const reversedNormalizedString = normalizedString.split('').reverse().join('');
+
+  return normalizedString === reversedNormalizedString;
+};
+
+// Строка является палиндромом
+checkIsPalindrome('топот');
+// Несмотря на разный регистр, тоже палиндром
+checkIsPalindrome('ДовОд');
+// Это не палиндром
+checkIsPalindrome('Кекс');
