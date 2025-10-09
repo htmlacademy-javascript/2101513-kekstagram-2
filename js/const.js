175b78f3794bdcec717ed0a390e7996bc7fc4bc3
Artur <102910077+ilyachever@users.noreply.github.com>
2025-10-09 22:20:21 +0400

    Добавляет файл со вспомогательными сообщениями

---
+++ js/const.js
@@ -0,0 +1,9 @@
+const ErrorMessage = {
+  STRING: 'Expected a not empty string, got a',
+  NUMBER: 'Expected a positive finite number, got a',
+  STRING_OR_NUMBER: 'Expected a not empty string or a positive finite number, got a',
+};
+
+export {
+  ErrorMessage,
+};
