d1e45e6d0a87f90499c7ed0ffabbd74011a16a30
Artur <102910077+ilyachever@users.noreply.github.com>
2025-10-09 22:29:35 +0400

    Добавляет функцию для извлечения цифр от 0 до 9
    Функция принимает строку или число, извлекает содержащиеся в аргументе цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в аргументе нет ни одной цифры, функция должна вернуть NaN:

--- js/functions.js
+++ js/functions.js
@@ -49,3 +49,31 @@ checkIsPalindrome('топот');
 checkIsPalindrome('ДовОд');
 // Это не палиндром
 checkIsPalindrome('Кекс');
+
+/**
+ * Извлекает из входящего параметра цифры от 0 до 9 и возвращает их в виде целого положительного числа
+ * @param {string | number} value
+ * @return {number | NaN} При наличии цифр озвращает целое положительное число, иначе NaN.
+ * @throws {Error} Если аргументы некорректного типа.
+ */
+const extractNumbers = (value) => {
+  const str = String(value);
+
+  if (!str.length) {
+    throw new Error(`${ErrorMessage.STRING} ${value}`);
+  }
+
+  const digitsWithoutOperators = str.match(/\d/g);
+
+  return digitsWithoutOperators ? Number(digitsWithoutOperators.join('')) : NaN;
+};
+
+extractNumbers('2023 год'); // 2023
+extractNumbers('ECMAScript 2022'); // 2022
+extractNumbers('1 кефир, 0.5 батона'); // 105
+extractNumbers('агент 007'); // 7
+extractNumbers('а я томат'); // NaN
+
+extractNumbers(2023); // 2023
+extractNumbers(-1); // 1
+extractNumbers(1.5); // 15
