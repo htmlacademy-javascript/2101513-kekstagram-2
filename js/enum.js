0e5b295bf38a91e6a2e7255fa80417a721b40aa0
Artur <102910077+ilyachever@users.noreply.github.com>
2025-10-19 17:28:59 +0400

    Добавляет дополнительное сообщение об ошибке

--- js/enum.js
+++ js/enum.js
@@ -2,6 +2,7 @@ const ErrorMessage = {
   STRING: 'Expected a not empty string, got a',
   NUMBER: 'Expected a positive finite number, got a',
   STRING_OR_NUMBER: 'Expected a not empty string or a positive finite number, got a',
+  ARRAY: 'Expected an array, got a',
 };
 
 export {
