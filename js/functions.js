917d4c6c3f06bf305a204c76a99e58a78d384c44
Artur <102910077+ilyachever@users.noreply.github.com>
2025-11-25 21:36:32 +0400

    Рефакторит JSDoc

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ import {timeToMinutes} from './utils.js';
 
 /**
  * Проверяет, укладывается ли встреча в рамки рабочего дня.
- * * @param {string} workdayStart - Время начала рабочего дня (например, '8:00').
+ * @param {string} workdayStart - Время начала рабочего дня (например, '8:00').
  * @param {string} workdayEnd - Время окончания рабочего дня (например, '17:30').
  * @param {string} meetingStart - Время начала встречи (например, '14:00').
  * @param {number} meetingDuration - Продолжительность встречи в минутах (например, 90).
